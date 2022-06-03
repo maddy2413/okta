e64ed0959a6184961ca59459707faa676229b5c4
maddy24 <pappachan24biju@gmail.com>
2022-06-03 08:10:46 +0530

    security code file

--- src/main/java/com/okta/examples/apiamhooks/ApiamHooksApplication.java
+++ src/main/java/com/okta/examples/apiamhooks/ApiamHooksApplication.java
@@ -16,8 +16,36 @@ public class ApiamHooksApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiamHooksApplication.class, args);
     }
-    
-    
 
-  
+/*
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                var securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                var collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+        return tomcat;
+    }
+
+    private Connector getHttpConnector() {
+        var connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
+
+*/
+
+
 }
